0f45c02734e0923a2c462e67771bcee5d6b3d665
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-14 03:11:27 +0600

    Задание 14.1.2

--- Q14/Program.cs
+++ Q14/Program.cs
@@ -6,20 +6,43 @@ namespace Q14
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var objects = new List<object>()
-{
-   1,
-   "Сергей ",
-   "Андрей ",
-   300,
-};
-            var selectedObjects = (from p in objects where p is string orderby p select p);
-            foreach (var Objects in selectedObjects)
-                Console.WriteLine(Objects);
-            foreach (var selectedObj in objects.Where(a => a is string).OrderBy(a => a))
-                Console.WriteLine(selectedObj);
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+
+            var bigCities = from russianCity in russianCities
+                            where russianCity.Population > 1000000
+                            orderby russianCity.Population descending
+                            select russianCity;
+
+            foreach (var bigCity in bigCities)
+                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+
+
+            var bigCitiess = russianCities.Where(c => c.Population > 1000000)
+  .OrderByDescending(c => c.Population);
+            var tenCities = from tenCitiess in russianCities where tenCitiess.Name.Length <= 10 orderby tenCitiess.Name.Length select tenCitiess;
+        }
+
+        // Создадим модель класс для города
+        public class City
+        {
+            public City(string name, long population)
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
         }
     }
 }
